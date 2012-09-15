556c21a155c01c512a288debe4cb1c1bedf02c9c
Holger Sindbaek <holgersindbaek@gmail.com>
2012-09-15 16:41:14 -0700

    testing

--- vendor/assets/javascripts/retina_image_tag.js
+++ vendor/assets/javascripts/retina_image_tag.js
@@ -2,3 +2,4 @@ if (document.cookie == "") {
   document.cookie = 'devicePixelRatio = ' + window.devicePixelRatio;
   if (document.cookie != "devicePixelRatio=1") window.location.reload();
 }
+console.log("test");
